5bf627f3c2f2f1495744ac74637ecd6ebcfc39b3
Eskil Abrahamsen Blomfeldt <eskil.abrahamsen-blomfeldt@nokia.com>
2013-11-20 11:02:37 +0100

    Fix license headers in example
    It's a part of QtAndroidExtras not QtWinExtras.
    
    Change-Id: I095d39e69dcd1c5362aa50a9e71a29e024fdab38
    Reviewed-by: Christian Stromme <christian.stromme@digia.com>

--- examples/androidextras/notification/main.cpp
+++ examples/androidextras/notification/main.cpp
@@ -1,10 +1,9 @@
-
 /****************************************************************************
 **
 ** Copyright (C) 2013 Digia Plc and/or its subsidiary(-ies).
 ** Contact: http://www.qt-project.org/legal
 **
-** This file is part of the QtWinExtras module of the Qt Toolkit.
+** This file is part of the QtAndroidExtras module of the Qt Toolkit.
 **
 ** $QT_BEGIN_LICENSE:LGPL$
 ** Commercial License Usage
