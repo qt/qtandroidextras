b80262f9d844b01c713c7df32754fc8194de900f
Assam Boudjelthia <assam.boudjelthia@qt.io>
2020-05-07 13:30:33 +0300

    Docs: Add a snippet for findClass usage
    Task-number: QTBUG-83038
    Change-Id: Ic789dfc55d73fba39ed2acf8f1b4376f772b7626
    Reviewed-by: BogDan Vatra <bogdan@kdab.com>
    (cherry picked from commit 9d11c9fe80e1c6801e1b378efeebf9127887a60a)
    Reviewed-by: Volker Hilsheimer <volker.hilsheimer@qt.io>

--- src/androidextras/jni/qandroidjnienvironment.cpp
+++ src/androidextras/jni/qandroidjnienvironment.cpp
@@ -93,6 +93,21 @@ QT_BEGIN_NAMESPACE
 
     Returns the class pointer or null if is not found.
 
+    A use case for this function is searching for a custom class then calling
+    its memeber method. The following code snippet create an instance of the
+    class \c CustomClass and then calls \c printFromJava() method:
+
+    \code
+    QAndroidJniEnvironment env;
+    jclass javaClass = env.findClass("org/qtproject/example/qtandroidextras/CustomClass");
+    QAndroidJniObject classObject(javaClass);
+
+    QAndroidJniObject javaMessage = QAndroidJniObject::fromString("findClass example");
+    classObject.callMethod<void>("printFromJava",
+                                 "(Ljava/lang/String;)V",
+                                 javaMessage.object<jstring>());
+    \badcode
+
     \since Qt 5.12
  */
 
